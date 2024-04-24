d4101184c61b37d40e6aec0dcfc06464a9515d0d
AndersonLLeite <anderson.tom.leite@gmail.com>
2024-04-23 21:29:55 -0300

    adicionado Dockerfile

--- backend-rupay/src/main/java/com/pc1/backendrupay/token/TokenRepository.java
+++ backend-rupay/src/main/java/com/pc1/backendrupay/token/TokenRepository.java
@@ -2,11 +2,13 @@ package com.pc1.backendrupay.token;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface TokenRepository extends JpaRepository<Token, Integer> {
 
     @Query(value = """
